631e99cc963519b636d990c5c5c0aa1f587e602a
danielvasic <dvasic@gmail.com>
2020-12-16 11:30:55 +0100

    Vježbe 09.12.2020.

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-require("db.php");
+require("model/db.php");
 
 if (isset($_POST["emailKorisnika"])){
     if ($_POST["emailKorisnika"] == "" || $_POST["lozinkaKorisnika"] == ""){
@@ -21,26 +21,9 @@ if (isset($_POST["emailKorisnika"])){
         }
     }
 }
+$naslov = "Prijava na sustav";
+include("static/header.php");
 ?>
-
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-
-    <title>Prijava na sustav e-dnevnik!</title>
-    <style>
-        html, body {
-            height:100% !important;
-        }
-    </style>
-  </head>
-  <body>
     <div class="container h-100">
         <div class="row align-items-center h-100">
             <div class="col shadow p-5">
@@ -64,7 +47,6 @@ if (isset($_POST["emailKorisnika"])){
             </div>
         </div>
     </div>
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
-  </body>
-</html>
+<?php
+include("static/footer.php");
+?>
--- register.php
+++ register.php
@@ -1,5 +1,5 @@
 <?php
-require ("db.php");
+require ("model/db.php");
 if (isset($_POST["imeKorisnika"])) {
     if ($_POST["imeKorisnika"] == ""  || $_POST["prezimeKorisnika"] == "" || $_POST["jmbgKorisnika"] == "" || $_POST["emailKorisnika"] == "" ||  $_POST["lozinkaKorisnika"] == "" || $_POST["pLozinkaKorisnika"] == "")
         $greska = "Nastala je greška niste unijeli sva polja.";
@@ -15,71 +15,53 @@ if (isset($_POST["imeKorisnika"])) {
         $rezultat = mysqli_query($konekcija, $SQL);
     }
 }
+$naslov = "Registracija na sustav";
+include("static/header.php");
 ?>
+<div class="container h-100">
+    <div class="row align-items-center h-100">
+        <div class="col shadow p-5">
+            <h5>Registracija na sustav e-dnevnik</h5>
+            <?php if (isset($greska)): ?>
+            <div class="alert alert-danger"><?php echo($greska) ?></div>
+            <?php endif ?>
+            <form method="POST" action="register.php">
+                <div class="form-group">
+                    <label>Ime:</label>
+                    <input type="text" class="form-control" name="imeKorisnika" placeholder="Unesite Vaše ime" />
+                </div>
 
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                <div class="form-group">
+                    <label>Prezime:</label>
+                    <input type="text" class="form-control" name="prezimeKorisnika" placeholder="Unesite Vaše prezime" />
+                </div>
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+                <div class="form-group">
+                    <label>Jedinstveni matični broj:</label>
+                    <input type="text" class="form-control" name="jmbgKorisnika" placeholder="Unesite Vaš JMBG" />
+                </div>
 
-    <title>Registracija na sustav e-dnevnik!</title>
-    <style>
-        html, body {
-            height:100% !important;
-        }
-    </style>
-  </head>
-  <body>
-    <div class="container h-100">
-        <div class="row align-items-center h-100">
-            <div class="col shadow p-5">
-                <h5>Registracija na sustav e-dnevnik</h5>
-                <?php if (isset($greska)): ?>
-                <div class="alert alert-danger"><?php echo($greska) ?></div>
-                <?php endif ?>
-                <form method="POST" action="register.php">
-                    <div class="form-group">
-                        <label>Ime:</label>
-                        <input type="text" class="form-control" name="imeKorisnika" placeholder="Unesite Vaše ime" />
-                    </div>
+                <div class="form-group">
+                    <label>E-mail adresa:</label>
+                    <input type="email" class="form-control" name="emailKorisnika" placeholder="Unesite Vašu email adresu" />
+                </div>
 
-                    <div class="form-group">
-                        <label>Prezime:</label>
-                        <input type="text" class="form-control" name="prezimeKorisnika" placeholder="Unesite Vaše prezime" />
-                    </div>
+                <div class="form-group">
+                    <label>Vaša lozinka:</label>
+                    <input type="password" class="form-control" name="lozinkaKorisnika" placeholder="Unesite Vašu lozinku" />
+                </div>
 
-                    <div class="form-group">
-                        <label>Jedinstveni matični broj:</label>
-                        <input type="text" class="form-control" name="jmbgKorisnika" placeholder="Unesite Vaš JMBG" />
-                    </div>
-
-                    <div class="form-group">
-                        <label>E-mail adresa:</label>
-                        <input type="email" class="form-control" name="emailKorisnika" placeholder="Unesite Vašu email adresu" />
-                    </div>
-
-                    <div class="form-group">
-                        <label>Vaša lozinka:</label>
-                        <input type="password" class="form-control" name="lozinkaKorisnika" placeholder="Unesite Vašu lozinku" />
-                    </div>
-
-                    <div class="form-group">
-                        <label>Ponovite Vašu lozinku:</label>
-                        <input type="password" class="form-control" name="pLozinkaKorisnika" placeholder="Ponovite Vašu lozinku" />
-                    </div>
-                    <p>Imate račun? Prijavite se <a  href="login.php">ovdje</a>.</p>
-                    
-                    <button type="submit" class="btn btn-primary">Pošalji obrazac</button>
-                </form>
-            </div>
+                <div class="form-group">
+                    <label>Ponovite Vašu lozinku:</label>
+                    <input type="password" class="form-control" name="pLozinkaKorisnika" placeholder="Ponovite Vašu lozinku" />
+                </div>
+                <p>Imate račun? Prijavite se <a  href="login.php">ovdje</a>.</p>
+                
+                <button type="submit" class="btn btn-primary">Pošalji obrazac</button>
+            </form>
         </div>
     </div>
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
-  </body>
-</html>
+</div>
+<?php
+include("static/footer.php");
+?>
